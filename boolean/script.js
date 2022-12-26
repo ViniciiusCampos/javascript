c43ea63b40b0c6208d1cf8c313242b357bd9588c
ViniciiusCampos <114677134+ViniciiusCampos@users.noreply.github.com>
2022-12-26 12:14:12 -0300

    a

---
+++ boolean/script.js
@@ -0,0 +1,35 @@
+/* let idade = 14;
+
+let verificacao = idade > 18;
+
+if (verificacao) {
+    console.log("Entrou no If");
+} else {
+    console.log("Nao Entrou no If");
+}
+ */
+
+////////////////// exercicios
+ 
+
+let x = 10;
+let y = 5;
+console.log(x > y)
+
+
+let w = "10";
+let z = 10;
+console.log(w === z);
+
+let a = 10;
+let b = 5;
+console.log(x !== y)
+
+
+let preco = 45;
+
+if (preco <= 45) {
+    console.log("A carne esta barata")
+} else {
+    console.log("A carne esta cara")
+}
