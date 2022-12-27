acce636adbe076a2c2eaf57489def057844b4740
ViniciiusCampos <114677134+ViniciiusCampos@users.noreply.github.com>
2022-12-27 12:20:23 -0300

    m

---
+++ function/script.js
@@ -0,0 +1,12 @@
+function maiorDeIdade(idade) {
+    if(idade >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let verificacao = maiorDeIdade(13)
+console.log(verificacao)
+
+
