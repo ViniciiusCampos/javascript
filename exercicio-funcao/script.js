7a7c1c1addeb7a4d772677a4faa9885681078ea6
ViniciiusCampos <114677134+ViniciiusCampos@users.noreply.github.com>
2022-12-28 12:27:57 -0300

    a

---
+++ exercicio-funcao/script.js
@@ -0,0 +1,16 @@
+function validar(usuario, senha) {
+    if (usuario === 'pedro' && senha === '123') {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+let usuario = "pedro";
+let senha = '123';
+let validacao = validar(usuario, senha);
+if (validacao) {
+    console.log('acesso concedido')
+} else {
+    console.log('acesso negado')
+}
--- function/script.js
+++ function/script.js
@@ -1,12 +1,1 @@
-function maiorDeIdade(idade) {
-    if(idade >= 18) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-let verificacao = maiorDeIdade(13)
-console.log(verificacao)
-
 
