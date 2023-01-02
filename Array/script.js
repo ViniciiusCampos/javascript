557cbfef2d8bf735b493a35f640d9e06ad7e900c
ViniciiusCampos <114677134+ViniciiusCampos@users.noreply.github.com>
2023-01-02 12:16:46 -0300

    4

---
+++ Array/script.js
@@ -0,0 +1,5 @@
+let nomes = [10, 9, 8];
+
+let lista = ['paulo', 'vini', nomes];
+
+console.log(lista)
---
+++ function/script.js
@@ -0,0 +1,10 @@
+function addSquares(a, b) {
+    function square(x) {
+        return x * x;
+    }
+
+    return square(a) + square(b);
+}
+
+
+console.log(addSquares(2, 10));
