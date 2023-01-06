8dc61419bbbbb29eff0afad5294d7e090955ec96
ViniciiusCampos <114677134+ViniciiusCampos@users.noreply.github.com>
2023-01-06 11:31:42 -0300

    66

---
+++ Doom/atributos.js
@@ -0,0 +1,17 @@
+function apertou() {
+  console.log("apertou")
+ 
+}
+
+function segurou() {
+
+    console.log('segurou')
+}
+
+function soltou() {
+
+    console.log('soltou')
+}
+
+const input = document.querySelector('input');
+document.addEventListener('keyup', soltou);
--- Doom/script.js
+++ Doom/script.js
@@ -2,7 +2,20 @@
 function clicou() {
     const teste = document.querySelector('#teste');
     const ul = teste.querySelector('ul');
+    let newLi = document.createElement("li");
+    newLi.innerText = "item adicionado";
 
- 
+    ul.prepend(newLi);
+
+    ul.children[0].append("(alterado)");
+
 }
 
+
+
+
+
+
+/* 
+
+    */
